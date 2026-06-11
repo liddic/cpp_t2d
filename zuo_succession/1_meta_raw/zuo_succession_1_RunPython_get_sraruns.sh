f39a3d5cf873cd335422a3a9913923a63c8d7941
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2026-06-11 23:12:05 +0930

    Add files via upload

---
+++ zuo_succession/1_meta_raw/zuo_succession_1_RunPython_get_sraruns.sh
@@ -0,0 +1,9 @@
+#!/bin/bash --login
+#SBATCH --account=pawsey1216
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=2:00:00
+
+python zuo_succession_1_get_sraruns.py
