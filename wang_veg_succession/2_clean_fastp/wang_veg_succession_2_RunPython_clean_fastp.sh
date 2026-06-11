9ed8be554c90c8f89db15e9ba4d33f58070b5fb9
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2026-06-11 23:03:06 +0930

    Add files via upload

---
+++ wang_veg_succession/2_clean_fastp/wang_veg_succession_2_RunPython_clean_fastp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash --login
+#SBATCH --account=pawsey1216
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=2:00:00
+
+python wang_veg_succession_2_clean_fastp.py
