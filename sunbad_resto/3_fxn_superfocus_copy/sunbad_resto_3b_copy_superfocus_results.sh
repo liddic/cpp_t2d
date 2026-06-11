9966580e3455b4c7037d90aa9f90e23a6f5e71d6
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2026-06-11 22:54:16 +0930

    Add files via upload

---
+++ sunbad_resto/3_fxn_superfocus_copy/sunbad_resto_3b_copy_superfocus_results.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+#SBATCH --account=pawsey1216
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=24:00:00
+
+cd /scratch/pawsey1216/cliddicoat/sunbad_resto/3_fxn_superfocus_copy
+
+awk '{print $1}' /scratch/pawsey1216/cliddicoat/sunbad_resto/2_clean_mgrast/sunbad-resto-mgp16379_metadata.tsv.txt > samples.txt
+
+## remove header
+awk 'NR>1' samples.txt > samples_noheader.txt
+
+
+for f in `cat samples_noheader.txt`; do
+
+mkdir -p superfocus_out_"$f"
+cp /scratch/pawsey1216/cliddicoat/sunbad_resto/3_fxn_superfocus/superfocus_out_"$f"/output_all_levels_and_function.xls /scratch/pawsey1216/cliddicoat/sunbad_resto/3_fxn_superfocus_copy/superfocus_out_"$f"/output_all_levels_and_function.xls
+
+done
