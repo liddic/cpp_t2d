63fc94787d03a69c1b9ba635072460cdafd1756b
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2026-06-11 22:36:45 +0930

    Add files via upload

---
+++ ref_genome_MOUSE/get_ncbi_ref_genome_MOUSE.sh
@@ -0,0 +1,12 @@
+#!/bin/bash --login
+#SBATCH --account=pawsey1216
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=24:00:00
+
+cd /scratch/pawsey1216/cliddicoat/ref_genome_MOUSE
+
+# Genome assembly GRCm39 - strain C57BL/6J - NCBI RefSeq assembly: GCF_000001635.27
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/635/GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.fna.gz
