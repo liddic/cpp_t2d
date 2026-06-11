8644252e1de3e16eb12beefb1c878a33104b2e17
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2026-06-11 23:06:08 +0930

    Add files via upload

---
+++ wang_veg_succession/4_read_counts/wang_veg_succession_4_read_counts_clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash --login
+#SBATCH --account=pawsey1216
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=24:00:00
+
+cd /scratch/pawsey1216/cliddicoat/wang_veg_succession/4_read_counts
+
+awk '{print $1}' /scratch/pawsey1216/cliddicoat/wang_veg_succession/1_meta_raw/SRR_Acc_List_wang_veg_succession.txt > samples_noheader.txt
+
+for f in `cat samples_noheader.txt`; do line_count=$(wc -l < /scratch/pawsey1216/cliddicoat/wang_veg_succession/2_clean_fastp/"$f"_R1.good.fastq) && result=$(($line_count / 4)) && echo "$f $result"; done > clean_r1_sequence_counts.txt
