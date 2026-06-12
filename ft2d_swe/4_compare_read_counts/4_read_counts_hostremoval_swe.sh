3880037d32c7ac3f4983869c0232aaa1c4dc3f39
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2026-06-12 21:21:01 +0930

    Add files via upload

---
+++ ft2d_swe/4_compare_read_counts/4_read_counts_hostremoval_swe.sh
@@ -0,0 +1,13 @@
+#!/bin/bash --login
+#SBATCH --account=pawsey1216
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=24:00:00
+
+cd /scratch/pawsey1216/cliddicoat/ft2d_swe/4_compare_read_counts
+
+#awk '{print $1}' /scratch/pawsey1216/cliddicoat/ft2d_swe/1_meta_raw/forslund-t2d-swe-run-list.txt > samples_noheader.txt
+
+for f in `cat samples_noheader.txt`; do line_count=$(wc -l < /scratch/pawsey1216/cliddicoat/ft2d_swe/2b_clean_hostremoval/"$f"_non_host.1.fastq) && result=$(($line_count / 4)) && echo "$f $result"; done > nonhost_r1_sequence_counts_swe.txt
