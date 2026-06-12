60d73431f7f977d39656a1d3e75dd7742937ee84
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2026-06-12 22:03:27 +0930

    Add files via upload

---
+++ ft2d_swe/cpp_analysis_5th/Run_R_cpp3d_onPawsey_t2d_swe_rarefied_5th.sh
@@ -0,0 +1,18 @@
+#!/bin/bash --login
+#SBATCH --account=pawsey1216
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=128
+#SBATCH --time=24:00:00
+
+module load r/4.4.1
+
+#export R_LIBS_USER=/software/projects/<project-id>/<user-name>/setonix/<DATE-TAG>/r/%v
+#export R_LIBS_USER=/software/projects/pawsey1216/cliddicoat/setonix/2024.05/r/4.4.1
+
+cd /scratch/pawsey1216/cliddicoat/ft2d_swe/cpp_analysis_5th
+
+# Execute the R script
+
+Rscript Rcode_cpp3d_onPawsey_t2d_swe_rarefied_5th.R
