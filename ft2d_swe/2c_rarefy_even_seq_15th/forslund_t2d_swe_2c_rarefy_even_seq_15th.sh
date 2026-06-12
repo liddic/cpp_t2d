8f94b78226d0316227dd876ce1535dc4b938821c
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2026-06-12 21:41:55 +0930

    Add files via upload

---
+++ ft2d_swe/2c_rarefy_even_seq_15th/forslund_t2d_swe_2c_rarefy_even_seq_15th.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+#SBATCH --account=pawsey1216
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=24:00:00
+
+cd /scratch/pawsey1216/cliddicoat/ft2d_swe/2c_rarefy_even_seq_15th
+export PATH=$PATH:/software/projects/pawsey1216/cliddicoat/conda_envs/_seqtk/bin
+
+awk '{print $1}' /scratch/pawsey1216/cliddicoat/ft2d_swe/2c_rarefy_even_seq_15th/keep_t2d_swe_list_15th.txt > samples_noheader.txt
+
+for f in `cat samples_noheader.txt`; do
+
+# seqtk sample -s100 input.fastq 5000 > output.fastq
+# 15% 
+# 5018997
+
+seqtk sample -s100 /scratch/pawsey1216/cliddicoat/ft2d_swe/2b_clean_hostremoval/"$f"_non_host.1.fastq 5018997 > /scratch/pawsey1216/cliddicoat/ft2d_swe/2c_rarefy_even_seq_15th/"$f"_non_host_rarefy_even.1.fastq
+
+done
