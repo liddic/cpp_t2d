c68a5d94b4926ab7fd9470ee7e412fd376d2689f
Craig Liddicoat <81080897+liddic@users.noreply.github.com>
2026-06-12 20:42:37 +0930

    Add files via upload

---
+++ ft2d_chn/3_fxn_superfocus_20th/forslund_t2d_chn_3_RunPython_fxn_superfocus_rarefied_20th.sh
@@ -0,0 +1,9 @@
+#!/bin/bash --login
+#SBATCH --account=pawsey1216
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=1:00:00
+
+python forslund_t2d_chn_3_fxn_superfocus_rarefied_20th.py
